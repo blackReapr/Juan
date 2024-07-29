6702bbe86860c71021d22342e8959ae34ce7bf3c
moaliyev <mehemmed05.aliyev@gmail.com>
2024-07-29 14:17:00 +0400

    feat: Account update added

--- Juan/Controllers/AccountController.cs
+++ Juan/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
 ﻿using Juan.Data;
+using Juan.Extensions;
+using Juan.Helpers;
 using Juan.Models;
 using Juan.ViewModels;
 using Microsoft.AspNetCore.Authorization;
@@ -8,28 +10,89 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Juan.Controllers;
 
-[Authorize(Roles = "memeber"), AllowAnonymous]
+[Authorize(Roles = "member")]
 public class AccountController : Controller
 {
     private readonly JuanDbContext _context;
     private readonly UserManager<AppUser> _userManager;
+    private readonly SignInManager<AppUser> _signInManager;
 
-    public AccountController(JuanDbContext context, UserManager<AppUser> userManager)
+    public AccountController(JuanDbContext context, UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
     {
         _context = context;
         _userManager = userManager;
+        _signInManager = signInManager;
     }
 
     public async Task<IActionResult> Index()
     {
-        if (!User.Identity.IsAuthenticated) return RedirectToAction("login", "authentication", new { returnUrl = "/account" });
         AppUser? appUser = await _userManager.FindByNameAsync(User.Identity.Name);
         if (appUser == null) return BadRequest();
         IEnumerable<Order> orders = await _context.Orders.Include(o => o.OrderItems).Where(o => o.UserId == appUser.Id).AsNoTracking().ToListAsync();
+        ViewBag.Orders = orders;
         AccountVM accountVM = new()
         {
-            Orders = orders,
+            Email = appUser.Email,
+            FirstName = appUser.FirstName,
+            LastName = appUser.LastName,
+            UserName = appUser.UserName
+
         };
         return View(accountVM);
+    }
+
+    [HttpPost, AutoValidateAntiforgeryToken]
+    public async Task<IActionResult> Index(AccountVM accountVM)
+    {
+        AppUser? appUser = await _userManager.FindByNameAsync(User.Identity.Name);
+        if (appUser == null) return BadRequest();
+        IEnumerable<Order> orders = await _context.Orders.Include(o => o.OrderItems).Where(o => o.UserId == appUser.Id).AsNoTracking().ToListAsync();
+        ViewBag.Orders = orders;
+        if (!ModelState.IsValid) return View(accountVM);
+
+        IEnumerable<string> errors = accountVM.Profile.VerifyFile();
+        if (errors.Any())
+        {
+            foreach (string error in errors) ModelState.AddModelError("Profile", error);
+            return View(accountVM);
+        }
+
+        string filename = await accountVM.Profile.SaveFileAsync();
+        string? oldFile = appUser.Profile == "default.jpg" ? null : appUser.Profile;
+
+        bool isPasswordCorrect = await _userManager.CheckPasswordAsync(appUser, accountVM.CurrentPassword);
+        if (!isPasswordCorrect)
+        {
+            ModelState.AddModelError("Error", "Incorrect Password");
+            return View(accountVM);
+        }
+
+        if (accountVM.Password != null)
+        {
+            IdentityResult result = await _userManager.ChangePasswordAsync(appUser, accountVM.CurrentPassword, accountVM.Password);
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors) ModelState.AddModelError("Error", error.Description);
+                return View(accountVM);
+            }
+        }
+
+        appUser.FirstName = accountVM.FirstName;
+        appUser.LastName = accountVM.LastName;
+        appUser.Email = accountVM.Email;
+        appUser.UserName = accountVM.UserName;
+        appUser.Profile = filename;
+
+        IdentityResult updateResult = await _userManager.UpdateAsync(appUser);
+        if (!updateResult.Succeeded)
+        {
+            foreach (var error in updateResult.Errors) ModelState.AddModelError("Error", error.Description);
+            return View(accountVM);
+        }
+
+        if (oldFile != null) DeleteFile.Delete(filename, "users");
+
+        await _signInManager.SignInAsync(appUser, true);
+        return View();
     }
 }
--- Juan/ViewModels/AccountVM.cs
+++ Juan/ViewModels/AccountVM.cs
@@ -1,8 +1,22 @@
-﻿using Juan.Models;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Juan.ViewModels;
 
 public class AccountVM
 {
-    public IEnumerable<Order> Orders { get; set; }
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+    [Required]
+    public string UserName { get; set; }
+    [Required, DataType(DataType.EmailAddress), EmailAddress]
+    public string Email { get; set; }
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    [DataType(DataType.Password), Compare(nameof(Password))]
+    public string RePassword { get; set; }
+    [Required, DataType(DataType.Password)]
+    public string CurrentPassword { get; set; }
+    public IFormFile? Profile { get; set; }
 }
